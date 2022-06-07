1419b25ca7e49de3f6f6ba29422bb1be5dd27108
sreeragpc <sreerag939@gmail.com>
2022-06-07 17:03:57 +0530

    white noise

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
+from settings import MEDIA_URL,MEDIA_ROOT
 
 urlpatterns = [
     path('customadmin/', include('secondadmin.urls')),
     path('', include('user.urls')),
     path('', include('mentor.urls')),
     path('admin/', admin.site.urls),
-]
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
