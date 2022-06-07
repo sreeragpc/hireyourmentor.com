095f928410b61cd40c5343454313b24e8c3be990
sreeragpc <sreerag939@gmail.com>
2022-06-07 17:21:08 +0530

    repath

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
-from django.conf.urls import url
 from settings import MEDIA_URL,MEDIA_ROOT
 
 urlpatterns = [
@@ -27,8 +26,8 @@ urlpatterns = [
     path('', include('user.urls')),
     path('', include('mentor.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
