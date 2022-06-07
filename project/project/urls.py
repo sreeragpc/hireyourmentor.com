3e076f41444136933ca6a55738120e3de68f562a
sreeragpc <sreerag939@gmail.com>
2022-06-07 17:23:27 +0530

    settings removed

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include,re_path
-from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
