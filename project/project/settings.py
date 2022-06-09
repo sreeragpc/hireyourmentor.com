39aee89f1da1ddac06a4919b980c32e8e752cd92
sreeragpc <sreerag939@gmail.com>
2022-06-09 13:31:54 +0530

    http s

--- project/project/settings.py
+++ project/project/settings.py
@@ -117,11 +117,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')] 
 STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
